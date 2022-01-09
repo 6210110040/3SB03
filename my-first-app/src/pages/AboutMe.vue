91ec78ac92ffa194c409a6a2afdc513793a6c5be
6210110040 <6210110040@psu.ac.th>
2022-01-09 23:24:16 +0700

    upload 3SB03

---
+++ my-first-app/src/pages/AboutMe.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div>
+      <q-btn-toggle
+        v-model="model"
+        class="my-custom-toggle"
+        no-caps
+        rounded
+        unelevated
+        toggle-color="primary"
+        color="white"
+        text-color="primary"
+        :options="[
+          {label: 'HOME', value: 'one'}
+        ]"
+        @click="$router.replace('/')"
+      />
+    </div>
+    <q-card class="my-card">
+      <img src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt86cd8572a30911aa/60cbfdb00ece0255888d895a/V_Ep_03_REFLECTION_Article-Header.jpg?auto=webp&disable=upscale&height=1055">
+
+      <q-card-section>
+        <div class="text-h6">สิ่งที่ฉันชอบทำเวลาว่าง</div>
+        <div class="text-subtitle2">นั่งเล่นเกมส์ และฟังเพลง</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img src="https://i.ytimg.com/vi/ua-iIRQDY8g/maxresdefault.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          ตัวละครที่ชอบ
+        </div>
+      </q-img>
+    </q-card>
+    
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      model: ref('one')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-custom-toggle
+  border: 1px solid #027be3
+</style>
---
+++ my-first-app/src/pages/Index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <q-carousel
+      arrows
+      animated
+      v-model="slide"
+      height="400px"
+    >
+      <q-carousel-slide name="first" img-src="https://scontent.fhdy1-1.fna.fbcdn.net/v/t1.6435-1/p240x240/77008180_2463143110672525_2571969559514841088_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=xtekeObscDkAX-tIbHq&_nc_ht=scontent.fhdy1-1.fna&oh=00_AT_HR17BtSQVeyOo475bRfBRtPz-t-rRjRJ4kqDw7-uzZg&oe=62016C85">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">First stop</div>
+          <div class="text-subtitle1">My Profile</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="second" img-src="https://scontent.fhdy1-1.fna.fbcdn.net/v/t1.6435-9/68751013_2367724860214351_5277194722815770624_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=174925&_nc_ohc=ynEfx5kWXCYAX9WNXIp&_nc_ht=scontent.fhdy1-1.fna&oh=00_AT8QQNG2u7Hw9HAW8ROUAW94k72sIIEy6VRCC-Pxyk2Zhg&oe=6201C5C1">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Second stop</div>
+          <div class="text-subtitle1">About me</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="third" img-src="https://scontent.fhdy1-1.fna.fbcdn.net/v/t1.6435-9/40203609_2118771275109712_6312911354508345344_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_ohc=-dkdPYXjtFQAX924OES&_nc_ht=scontent.fhdy1-1.fna&oh=00_AT-mm4SM72F0aCJjCH7DkdVeCUSiG3g_Xo8eJtvk791ljA&oe=6200F652">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Third stop</div>
+          <div class="text-subtitle1">End credit</div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+    <div>
+      <q-btn-toggle
+        v-model="model"
+        class="my-custom-toggle"
+        no-caps
+        rounded
+        unelevated
+        toggle-color="primary"
+        color="white"
+        text-color="primary"
+        :options="[
+          {label: 'About_Me', value: 'two'}
+        ]"
+        @click="$router.replace('/aboutme')"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slide: ref('first')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 12px
+  color: white
+  background-color: rgba(0, 0, 0, .3)
+</style>
